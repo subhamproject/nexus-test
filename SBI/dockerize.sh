bd3cac569a927e47692ac3911a18cfdb3a48b14e
Subham Mandal <subhammandal01@gmail.com>
2019-05-02 22:21:52 +0530

    Update dockerize.sh

--- SBI/dockerize.sh
+++ SBI/dockerize.sh
@@ -36,5 +36,5 @@ esac
 services=$(cat $(dirname $0)/service-manifest.txt)
 for s in $services
 do
-docker build -t ${s}:$tag --build-arg VERSION=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//') -f $(dirname $0)/docker/$dockerfile .
+docker build -t ${s}:$tag --build-arg VERSION=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//'|cut -d'-' -f1) -f $(dirname $0)/docker/$dockerfile .
 done
