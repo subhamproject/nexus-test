80082caa49beeb45eedbac1c73d97f67c158763d
Subham Mandal <subhammandal01@gmail.com>
2019-07-17 17:46:34 +0530

    Update build.sh

--- SBI/build.sh
+++ SBI/build.sh
@@ -12,4 +12,3 @@ echo "Running as UID=$USERID, GID=$GROUPID on branch $BRANCH_NAME"
 cd $(dirname $0)
 #[ $BRANCH_NAME == "master" ] && export NEXUS_REPO=nexus-release || export NEXUS_REPO=nexus-snapshot
 docker-compose -f test-bed.yml run --name maven-${BUILD_NUMBER} --rm -w "$WORKSPACE" -e NEXUS_REPO=$NEXUS_REPO --entrypoint "mvn -s settings.xml clean packages" maven-app-build
-echo $?
